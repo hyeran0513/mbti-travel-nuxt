8ed7db96cbf5441f628535299e90be31cb7e88a4
hyeran0513 <hr_0513@naver.com>
2024-06-13 20:47:31 +0900

    FEAT:  nuxt 기본 폴더 구조

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout">
+    <LayoutHeader />
+
+    <div class="content">
+      <slot />
+    </div>
+
+    <LayoutFooter />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  vite: {
+    css: {
+      preprocessorOptions: {
+          scss: {
+              additionalData: '@import "@/assets/scss/_mixins.scss";',
+          },
+      },
+    },
+  },
+  css : [
+    "@/assets/scss/main.scss"
+  ],
+  modules: ['nuxt-font-loader'],
+  fontLoader: {
+    local: [
+      {
+        src: '/fonts/elice/EliceDigitalBaeum_Regular.woff2',
+        family: 'EliceDigitalBaeum',
+        weight: '400',
+        style: 'normal'
+      },
+      {
+        src: '/fonts/elice/EliceDigitalBaeum_Bold.woff2',
+        family: 'EliceDigitalBaeum',
+        weight: 'bold',
+        style: 'normal'
+      },
+      {
+        src: '/fonts/icomoon/icomoon.ttf',
+        family: 'iconFont',
+        weight: 'normal',
+        style: 'normal',
+        display: 'block',
+      }
+    ]
+  }
 })
